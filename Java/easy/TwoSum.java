f5d40902dc1c0efd1bfd4e4362ba365ccd295d86
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-21 23:11:36 +0100

    Solve the problem with O(n) using enumerate with python and hashmap with java

--- Java/easy/TwoSum.java
+++ Java/easy/TwoSum.java
@@ -1,15 +1,45 @@
 package easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int k = nums.length;
+
+//         for (int i = 0; i < k; i++) {
+//             for (int j = i + 1; j < k; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[] { i, j };
+//                 }
+//             }
+//         }
+
+//         return new int[] {};
+//     }
+
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+
+//         int[] nums = { 2, 7, 11, 15 };
+//         int target = 9;
+//         int[] result = sol.twoSum(nums, target);
+//         System.out.println(result[0] + " " + result[1]);
+//     }
+// }
+
+//  Time complexity = O(n)
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int k = nums.length;
+        Map<Integer, Integer> seen = new HashMap<>();
 
-        for (int i = 0; i < k; i++) {
-            for (int j = i + 1; j < k; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[] { seen.get(complement), i };
             }
+            seen.put(nums[i], i);
         }
 
         return new int[] {};
--- Python/easy/TwoSum.py
+++ Python/easy/TwoSum.py
@@ -1,14 +1,28 @@
 
 # Time Complexity = O(n²)
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        k = len(nums)
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         k = len(nums)
         
-        for i in range(k):
-            for j in range(i + 1, k):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+#         for i in range(k):
+#             for j in range(i + 1, k):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
 
 
 # Time complexity = O(n)
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+
+        return []
+
+print(Solution().twoSum([2,7,11,15], 9))
